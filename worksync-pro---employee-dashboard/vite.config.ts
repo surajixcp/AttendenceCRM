c0220cebb6ba3b1aeddb3dc3e590ae866172514e
Surajixcp <surajgiri2002x@gmail.com>
2026-01-24 10:58:24 +0530

    new

--- worksync-pro---employee-dashboard/vite.config.ts
+++ worksync-pro---employee-dashboard/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      open: 'http://localhost:3000',
       proxy: {
         '/api': {
           target: 'http://127.0.0.1:5000',
